fb8d48dc987eb2d1609be38bd07fc99c8ef25cc5
Justin Marchi <marchij86@gmail.com>
2020-05-19 21:44:40 -0400

    Fixed login functionality, began AJAX call

--- public/js/members.js
+++ public/js/members.js
@@ -1,3 +1,5 @@
+// var dotenv = require("dotenv").config();
+var bookTitle = "";
 
 $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
@@ -5,4 +7,28 @@ $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.email);
   });
+
+  $("#bookSearchBtn").on("click", function(event) {
+    event.preventDefault();
+
+    bookTitle = $("#bookSearch").val().trim();
+    if (bookTitle ===""){
+      return false;
+    } else {
+      renderBookList();
+    }
+  });
+
+  async function renderBookList() {
+    var bookURL = "https://cors-anywhere.herokuapp.com/https://www.goodreads.com/search.xml?key=UBE8gwrtQDEM0cjUh0luQ&q=" + bookTitle;
+
+    var response = await $.ajax({
+      url: bookURL,
+      method: "GET"
+    })
+    console.log(response);
+    
+  }
 });
+
+
